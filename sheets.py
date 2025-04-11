3c56b330749656cd876e2d5fd74c0b748f1d1b9d
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-04-11 10:48:03 +0200

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import requests
-from io import StringIO
 
 def leer_kpis():
     sheet_id = "1-40eCYIUj8yKBC1w55ukAO45lLnL7gEm1-p_OLkL8Lk"
-    gid = "0"  # Puedes cambiar esto si necesitas leer otra pestaña
+    gid = "1076160199"  # Pestaña: KPIs_DiariosS
 
-    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
+    SHEET_URL = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
 
-    response = requests.get(url)
+    response = requests.get(SHEET_URL)
+
     if response.status_code != 200:
         raise Exception(f"HTTP Error {response.status_code}: {response.reason}")
 
+    # Lee el CSV directamente desde la respuesta
+    from io import StringIO
     df = pd.read_csv(StringIO(response.text))
+
     return df.to_dict(orient="records")
