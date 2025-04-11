4ebb99602fa84f2d3a6ca338f0daa45d015580b3
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-04-11 10:03:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from sheets import leer_kpis
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/kpis")
 def obtener_kpis():
-    return leer_kpis()
+    try:
+        data = leer_kpis()
+        return {"kpis": data}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
