117c1e95f1f6088946a09efba7612a56fe74bdd9
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-04-11 10:26:41 +0200

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-# Enlace directo a la hoja pública
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1-40eCYIUj8yKBC1w55ukAO45lLnL7gEm1-p_OLkL8Lk/export?format=csv&gid=0"
-
 def leer_kpis():
+    sheet_id = "1-40eCYIUj8yKBC1w55ukAO45lLnL7gEm1-p_OLkL8Lk"
+    gid = "0"  # Puedes cambiar esto si necesitas otra pestaña
+    SHEET_URL = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&id={sheet_id}&gid={gid}"
+
     df = pd.read_csv(SHEET_URL)
     return df.to_dict(orient="records")
+
