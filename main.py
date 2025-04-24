f929e77081bcb126657497a4d88eed513b81c017
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-04-24 17:12:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, HTTPException
 from sheets import leer_kpis, analizar_salon
 
 app = FastAPI()
 
 @app.get("/kpis")
-def obtener_datos_crudos(
-    year: int = Query(None), 
-    nsemana: int = Query(None), 
-    codsalon: int = Query(None)
-):
-    df = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo="semana")
-    return {"kpis": df.to_dict(orient="records")}
-
+def obtener_datos_crudos(year: int, nsemana: int, codsalon: int):
+    try:
+        df = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo="semana")
+        return {"kpis": df.to_dict(orient="records")}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/kpis/salon/analisis")
-def obtener_analisis_salon(
-    year: int, 
-    nsemana: int, 
-    codsalon: int
-):
-    df = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo="semana")
+def obtener_analisis_salon(year: int, nsemana: int, codsalon: int):
     try:
+        df = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo="semana")
         resultado = analizar_salon(df)
         return {"analisis": resultado}
     except Exception as e:
-        return {"analisis": {"error": str(e)}}
+        raise HTTPException(status_code=500, detail=str(e))
