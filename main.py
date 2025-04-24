facaa13388b69f49a860225cb3519fe0fc2c8b13
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-04-24 15:45:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ app = FastAPI()
 def obtener_datos_crudos(
     year: int = Query(...),
     nsemana: int = Query(...),
-    codsalon: int = Query(...)
+    codsalon: int = Query(...),
+    tipo: str = Query("semana")
 ):
     try:
-        df = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo="semana")
-        return {"datos": df.to_dict(orient="records")}
+        df = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo=tipo)
+        return {"filas": len(df), "datos": df.to_dict(orient="records")}
     except Exception as e:
         return JSONResponse(
             status_code=500,
-            content={"error": str(e), "trace": traceback.format_exc()}
+            content={
+                "error": str(e),
+                "trace": traceback.format_exc()
+            }
         )
