ecfc312557cde8e8eac2d003b7b25787c2580fcf
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-04-24 08:48:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ app = FastAPI()
 
 @app.get("/kpis")
 def obtener_kpis(
-    year: int = Query(..., description="Año, por ejemplo 2025"),
-    nsemana: int = Query(..., description="Número de semana"),
-    codsalon: int = Query(..., description="Código de salón")
+    year: int = Query(None, description="Año, por ejemplo 2025"),
+    nsemana: int = Query(None, description="Número de semana"),
+    codsalon: int = Query(None, description="Código de salón"),
+    tipo: str = Query("semana", description="Tipo de hoja: semana, trabajadores, mensual")
 ):
     try:
-        data = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon)
+        data = leer_kpis(year=year, nsemana=nsemana, codsalon=codsalon, tipo=tipo)
         return {"kpis": data}
     except Exception as e:
         return JSONResponse(
