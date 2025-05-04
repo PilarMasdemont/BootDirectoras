266ac30f005542b1a46116c716867a45e8747443
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-05-04 11:00:44 +0200

    Create __init__.py

---
+++ funciones/__init__.py
@@ -0,0 +1,6 @@
+from .kpis import consultar_kpis
+from .analizar_salon import analizar_salon
+from .analizar_trabajadores import analizar_trabajadores
+from .explicar_kpi import explicar_kpi
+from .explicar_variacion import explicar_variacion
+from .sugerencias_mejora import sugerencias_mejora
