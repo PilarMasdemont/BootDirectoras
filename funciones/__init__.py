54eb84e3e5bb6d06b4016ed58cf72ac422335445
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-05-21 09:48:08 +0200

    Update __init__.py

--- funciones/__init__.py
+++ funciones/__init__.py
@@ -1,6 +1,4 @@
-from .kpis import consultar_kpis
-from .analizar_salon import analizar_salon
-from .analizar_trabajadores import analizar_trabajadores
-from .explicar_kpi import explicar_kpi
-from .explicar_variacion import explicar_variacion
-from .sugerencias_mejora import sugerencias_mejora
+# funciones/__init__.py
+# Archivo de inicialización del módulo 'funciones'.
+# Actualmente vacío.
+
