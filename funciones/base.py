a0835ff5dbcd31b84df11a5d6e789055019b475a
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-05-21 09:40:14 +0200

    Delete funciones/base.py

--- funciones/base.py
+++
@@ -1,9 +0,0 @@
-# funciones/base.py
-import pandas as pd
-from funciones.utils import cargar_hoja
-
-def leer_kpis(year, nsemana, codsalon, tipo="semana"):
-    """Carga y filtra los KPIs desde Google Sheets según parámetros"""
-    df = cargar_hoja(tipo)
-    df = df[(df['año'] == year) & (df['nsemana'] == nsemana) & (df['cod_salon'] == codsalon)]
-    return df
