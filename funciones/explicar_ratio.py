00ed193cc7fd04d6b68cb0223a3f5a5a1a97ae7d
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-05-26 17:27:28 +0200

    Create explicar_ratio.py

---
+++ funciones/explicar_ratio.py
@@ -0,0 +1,13 @@
+from explicar_ratio_diario import explicar_ratio_diario
+from explicar_ratio_empleados import explicar_ratio_empleados
+from intencion import clasificar_intencion
+
+def explicar_ratio(codsalon: str, fecha: str, mensaje_usuario: str) -> str:
+    try:
+        tipo = clasificar_intencion(mensaje_usuario)
+        if tipo == "individual":
+            return explicar_ratio_empleados(codsalon, fecha)
+        else:
+            return explicar_ratio_diario(codsalon, fecha)
+    except Exception as e:
+        return f"❌ Error al clasificar la intención del mensaje: {str(e)}"
