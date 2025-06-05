9c3f59947ef70a87d1b697e5632b55cfbf0e48d6
PilarMasdemont <peluqueriaspilarmasdemont@gmail.com>
2025-06-05 07:17:40 +0200

    Update chat_router.py

--- routes/chat_router.py
+++ routes/chat_router.py
@@ -5,8 +5,6 @@ from config import setup_environment, openai_client
 from extractores import detectar_kpi, extraer_fecha_desde_texto, extraer_codempleado, extraer_codsalon
 from funciones.explicar_ratio import explicar_ratio
 from funciones.explicar_ratio_empleados import explicar_ratio_empleados
-from funciones.explicar_ratio_empleado_individual import explicar_ratio_empleado_individual
-from intenciones.explicar_ratio.handler import explicar_ratio_diario
 from funciones.explicar_ratio_mensual import explicar_ratio_mensual
 from funciones.explicar_ratio_semanal import explicar_ratio_semanal
 from routes.chat_flujo_empleados import manejar_flujo_empleados
@@ -14,6 +12,8 @@ from routes import chat_functions
 from google_sheets_session import cargar_sesion, guardar_sesion
 from manejar_peticion_chat import manejar_peticion_chat
 from funciones.explicar_producto import explicar_producto
+from intenciones.explicar_ratio.ratio_diario import explicar_ratio_diario
+from intenciones.explicar_ratio.ratio_empleado import explicar_ratio_empleado_individual
 
 
 import json
